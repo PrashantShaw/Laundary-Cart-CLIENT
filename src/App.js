2ad680a3e96dda34288ecf2f7fb63c441fb09840
Ashish kumar <ashish056244@gmail.com>
2022-11-23 14:31:06 +0530

    created login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Homepage/Nav';
+import Middle from './components/Homepage/Homepage';
+import { Footer } from './components/Homepage/Footer';
+import Register from './components/Register/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    {/* <Middle/> */}
+    <Register/>
+    <Footer/>
+    </>
   );
 }
 
---
+++ src/components/summery.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const summery = () => {
+  return (
+    <div>summery</div>
+  )
+}
+
+export default summery
