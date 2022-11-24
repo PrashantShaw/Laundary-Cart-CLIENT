ca5ad205e93b24d2fecc3b54f9f84b27338d07e5
Deepthimokarala <deepthipendyala97@gmail.com>
2022-11-24 14:30:28 +0530

    creatingorderpage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Header from './components/Homepage/Nav';
 import Middle from './components/Homepage/Homepage';
 import { Footer } from './components/Homepage/Footer';
 import Register from './components/Register/Register';
-
+import TopNavBar from './components/CreatingOrder/CreatingOrder';
+import Product from './components/OrdersList/OrdersList';
 function App() {
   return (
     <>
@@ -11,8 +12,11 @@ function App() {
     {/* <Middle/> */}
     <Register/>
     <Footer/>
+    <Product/>z
+    <TopNavBar />
     </>
   );
 }
+
 
 export default App;
---
+++ src/components/CreatingOrder/CreatingOrder.js
@@ -0,0 +1,56 @@
+import React ,{navigate} from "react";
+import home  from "../Assests/home.svg"
+import list from "../Assests/list.png"
+import more from "../Assests/more.png"
+import './CreatingOrder.css';
+
+const TopNavBar = () => {
+  const user= ""
+  //const navigate =useNavigate();
+    return (
+    <>
+      <div className="header_container">
+        <div className="header_name">LAUNDRY</div>
+        <div className="header_links">
+          <div>Pricing</div>
+          <div>Career</div>
+          <div
+            style={{
+              display: "flex",
+
+              color: "white",
+              fontWeight: "600",
+            }}
+            className="user_name"
+          >
+            {user ? user.name : "User Name"}
+
+          </div>
+        </div>
+      </div>
+      
+
+      <div className="sidebar_container">
+        <img src={home} alt="home" />
+        <div>
+        <img src={more} alt="more" />
+        </div>
+        <div className="container">
+        <img src={list} alt="list" />
+       </div>
+      </div>
+      {/* <h2 className="heading">Create order</h2> */}
+              <button
+                onClick={() => {
+                  navigate("/createOrder");
+                }}
+                className="create-button"
+              >
+                Create
+              </button>
+              
+    </>
+  );
+};
+
+export default TopNavBar;
